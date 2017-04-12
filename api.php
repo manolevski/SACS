6aefbdac69af8c4309c5b8fd41a58fbe563b17af
Anastas Manolevski <anjoto@abv.bg>
2017-04-12 14:14:27 -0400

    Added encryption
    Added MCRYPT encryption in case someone tries to change the response.

--- api.php
+++ api.php
@@ -1,12 +1,13 @@
 <?php
  
 $token= $_GET['token']; 
+$key = 'SuperSecretKey13';
 
 $string = file_get_contents("user_data.json");
 $json = json_decode($string, true); // decode the JSON into an associative array
 
 foreach ($json as $field) {
   if($field['token'] == $token)
-    echo json_encode($field);
+    echo mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, json_encode($field), MCRYPT_MODE_ECB);
 }
 ?>
