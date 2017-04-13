e7288b1ef651ce5075c62b19d91ca10828a06d9d
Anastas Manolevski <anjoto@abv.bg>
2017-04-13 13:23:47 -0400

    Extracted the encryption in function

--- service/api.php
+++ service/api.php
@@ -11,14 +11,17 @@ foreach ($json as $field) {
 	if($field['token'] == $token){
 		$field['timestamp'] = time();
 		$text = json_encode($field);
-		$block = mcrypt_get_block_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
-		$padding = $block - (strlen($text) % $block);
-		$text .= str_repeat(chr($padding), $padding);
-		
-		$crypttext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $text, MCRYPT_MODE_CBC, $iv);
-		$crypttext64=base64_encode($crypttext);
-		
-		echo $crypttext64;
+		echo encrypt($text, $key, $iv);
 	}
+}
+
+function encrypt($text, $key, $iv) {
+    $block = mcrypt_get_block_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
+	$padding = $block - (strlen($text) % $block);
+	$text .= str_repeat(chr($padding), $padding);
+	
+	$crypttext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $text, MCRYPT_MODE_CBC, $iv);
+	$crypttext64=base64_encode($crypttext);
+    return $crypttext64;
 }
 ?>
