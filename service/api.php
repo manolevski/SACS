b9ee77ba6c19a650776289491089aa5f6b884848
manolevski <anastas.manolevski@gmail.com>
2017-04-12 22:32:56 -0400

    Formatting

--- service/api.php
+++ service/api.php
@@ -9,15 +9,15 @@ $json = json_decode($string, true); // decode the JSON into an associative array
 
 foreach ($json as $field) {
   if($field['token'] == $token){
-	$text = json_encode($field);
-	$block = mcrypt_get_block_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
-	$padding = $block - (strlen($text) % $block);
-	$text .= str_repeat(chr($padding), $padding);
-	
-	$crypttext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $text, MCRYPT_MODE_CBC, $iv);
-	$crypttext64=base64_encode($crypttext);
-	  
-    echo $crypttext64;
+		$text = json_encode($field);
+		$block = mcrypt_get_block_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
+		$padding = $block - (strlen($text) % $block);
+		$text .= str_repeat(chr($padding), $padding);
+		
+		$crypttext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $text, MCRYPT_MODE_CBC, $iv);
+		$crypttext64=base64_encode($crypttext);
+		
+		echo $crypttext64;
   }
 }
 ?>
